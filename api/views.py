1010df2f1447a30de372a7c4f4b40aa95cea2d71
viktor ojemeni <58849181+drex007@users.noreply.github.com>
2022-03-05 06:23:11 +0000

    feat: added all the endpoints needed

--- api/views.py
+++ api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response 
+from .models import Note
+from .serializers import NoteSerializer
+
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -39,3 +43,47 @@ def getRoutes(request):
     
      ]
     return Response(routes)
+
+
+# Get all the notes
+@api_view(['GET'])
+def getNotes(request):
+    notes = Note.objects.all()
+    serialized = NoteSerializer(notes, many= True)
+    return Response(serialized.data)
+
+# Get all the Singlenote
+@api_view(['GET'])
+def getNote(request, pk):
+    note = Note.objects.get(id = pk)
+    serialized = NoteSerializer(note, many= False)
+    return Response(serialized.data)
+
+# Create a Note
+@api_view(['POST'])
+def createNote(request):
+    data = request.data 
+    newdata = Note.objects.create(
+        body = data['body']
+    )
+    serialized = NoteSerializer(newdata, many= False)
+    return Response(serialized.data)
+
+#Update a note 
+@api_view(['PUT'])
+def updateNote(request, pk):
+    data = request.data 
+    note = Note.objects.get(id=pk)
+    serialized = NoteSerializer(note, data = data)
+    if serialized.is_valid():
+        serialized.save()
+    return Response(serialized.data)
+
+
+#Delete a Note
+@api_view(['DELETE'])
+def deleteNote(request, pk):
+    note = Note.objects.get(id=pk)
+    note.delete()
+    return Response("Note is  deleted")
+ 
